e13b08dcec0f29a13b58cc1cc7659d7cebec0d87
JennieTran <146484566+jtmtran@users.noreply.github.com>
2025-05-06 23:48:34 -0400

    Add files via upload

--- trendlens.py
+++ trendlens.py
@@ -287,7 +287,6 @@ st.markdown(
     "<a href='https://github.com/jtmtran/reddit_trending_realtime' target='_blank'>"
     "<img style='position: absolute; top: 0; right: 0; border: 0;' "
     "src='https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149' "
-    "alt='Fork me on GitHub'>"
     "</a>",
     unsafe_allow_html=True
 )
